e35acaa10088c4a21aeb2962c6d8a00dd82e4331
Scott Mahr <essmahr@gmail.com>
2017-06-12 22:33:50 -0700

    Add styles for spaces

--- src/app/components/EntityListItem/index.js
+++ src/app/components/EntityListItem/index.js
@@ -7,11 +7,15 @@ import CopyButton from '../CopyButton';
 const EntityListItem = function(props) {
   const { name, html, css, onButtonClick } = props;
 
+  const styleName = props.space
+    ? `entity-space-${props.space}`
+    : 'entity-symbol';
+
   return (
     <li styleName="entity-list-item-outer">
       <div styleName="entity-list-item-inner">
         <div styleName="entity-name">{name}</div>
-        <div styleName="entity-symbol" dangerouslySetInnerHTML={{__html: html}}></div>
+        <div styleName={styleName}><span dangerouslySetInnerHTML={{__html: html}}></span></div>
         <CopyButton onClick={props.onButtonClick} type="css" content={css} styles={styles} />
         <CopyButton onClick={props.onButtonClick} type="html" content={html} styles={styles} />
       </div>
