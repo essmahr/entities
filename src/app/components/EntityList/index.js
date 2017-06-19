77243d2e65ecf69c2276cdc0d9c566d7b2b35f44
Scott Mahr <essmahr@gmail.com>
2017-06-18 22:31:22 -0700

    Add empty state

--- src/app/components/EntityList/index.js
+++ src/app/components/EntityList/index.js
@@ -3,6 +3,12 @@ import CSSModules from 'react-css-modules';
 import EntityListItem from '../EntityListItem';
 import styles from './style.scss';
 
+const Empty = function(props) {
+  return (
+    <div className={props.styles.empty}>No Matches</div>
+  );
+}
+
 const EntityList = function(props) {
   const entities = props.entities.map((entity, idx) => {
     return <EntityListItem key={`${entity.html}-${idx}`} {...entity} onButtonClick={props.onButtonClick} />
@@ -10,9 +16,11 @@ const EntityList = function(props) {
 
   return (
     <div styleName="entities-container-outer">
-      <ul styleName="entities-container">
-        {entities}
-      </ul>
+      {
+        entities.length > 0
+          ? <ul styleName="entities-container">{entities}</ul>
+          : <Empty styles={props.styles} />
+      }
     </div>
   );
 }
